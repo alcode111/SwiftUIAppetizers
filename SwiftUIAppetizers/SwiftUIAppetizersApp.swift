1993b7aaa3576ea4e6a5731ee155ab5cc19ad33b
alcode111 <al.code.111@gmail.com>
2024-03-31 13:29:44 +0200

    Initial Commit

---
+++ SwiftUIAppetizers/SwiftUIAppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppetizersApp.swift
+//  SwiftUIAppetizers
+//
+//  Created by Ismaïl LP on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
