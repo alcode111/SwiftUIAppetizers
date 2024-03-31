135a3cf4a6d78ae33ae230ae3c3ed1d7c6e38c88
alcode111 <al.code.111@gmail.com>
2024-03-31 15:36:25 +0200

    Refactor our list cell

--- SwiftUIAppetizers/AppetizerListView.swift
+++ SwiftUIAppetizers/AppetizerListView.swift
@@ -11,24 +11,7 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationStack {
             List(MockData.appetizers) { appetizer in
-                HStack {
-                    Image("asian-flank-steak")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120, height: 90)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(appetizer.name)
-                            .font(.title2)
-                            .fontWeight(.medium)
-                        
-                        Text("€\(appetizer.price, specifier: "%.2f")")
-                            .foregroundStyle(.secondary)
-                            .fontWeight(.semibold)
-                    }
-                    .padding(.leading)
-                }
+                AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
         }
