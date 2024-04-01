dabd4cba166cef6a9ed83b06aba518ad9c18e0ef
alcode111 <al.code.111@gmail.com>
2024-04-01 15:16:15 +0200

    Create Network Manager

---
+++ SwiftUIAppetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SwiftUIAppetizers
+//
+//  Created by Ismaïl LP on 01/04/2024.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
---
+++ SwiftUIAppetizers/NetworkManager.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkManager.swift
+//  SwiftUIAppetizers
+//
+//  Created by Ismaïl LP on 31/03/2024.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let appetizerURL = baseURL + "appetizers"
+    
+    private init() {}
+    
+    func getAppetizers(completed: @escaping(Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+            }
+        }
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            completed(.failure(.invalidResponse))
+            return
+        }
+        
+        guard let data = data else {
+            completed(.failure(.invalidData))
+            return
+        }
+    }
+}
