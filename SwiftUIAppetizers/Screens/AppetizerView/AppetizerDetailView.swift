9852cfb4971faea0e7f14ee1dd48ddda083fde25
alcode111 <al.code.111@gmail.com>
2024-04-25 20:04:22 +0200

    Build AppetizerDetailView

--- SwiftUIAppetizers/Screens/AppetizerView/AppetizerDetailView.swift
+++ SwiftUIAppetizers/Screens/AppetizerView/AppetizerDetailView.swift
@@ -12,7 +12,91 @@ struct AppetizerDetailView: View {
     let appetizer: Appetizer
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.asianFlankSteak)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.calories)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.protein)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .frame(width: 260, height: 50)
+                        .foregroundStyle(.white)
+                        .background(.accent)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.bottom, 30)
+            }
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 40)
+        .overlay(Button {
+            print("dimiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30)
+                    .foregroundStyle(.white.opacity(0.6))
+                
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 44, height: 44)
+                    .foregroundStyle(.black)
+            }
+        }, alignment: .topTrailing)
     }
 }
 
