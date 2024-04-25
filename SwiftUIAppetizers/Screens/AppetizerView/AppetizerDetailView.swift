0235e42b87ed3821acdfec8a67a24d6e759dc2af
alcode111 <al.code.111@gmail.com>
2024-04-25 18:44:54 +0200

    Create ImageLoader, reorganize code, start building AppetizerDetailView

---
+++ SwiftUIAppetizers/Screens/AppetizerView/AppetizerDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  AppetizerDetailView.swift
+//  SwiftUIAppetizers
+//
+//  Created by Ismaïl LP on 25/04/2024.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+}
