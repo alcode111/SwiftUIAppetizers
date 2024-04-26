7688b1997889b1ef7c7dd06e8144ccb957cc1ecf
alcode111 <al.code.111@gmail.com>
2024-04-26 15:20:59 +0200

    Create a User Model, update Account views accordingly

--- SwiftUIAppetizers/Screens/AccountView/AccountView.swift
+++ SwiftUIAppetizers/Screens/AccountView/AccountView.swift
@@ -15,13 +15,13 @@ struct AccountView: View {
         NavigationStack {
             Form {
                 Section(header: Text("Personal information")) {
-                    TextField("First Name", text: $viewModel.firstName)
-                    TextField("Last Name", text: $viewModel.lastName)
-                    TextField("Email", text: $viewModel.email)
+                    TextField("First Name", text: $viewModel.user.firstName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
+                    TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
-                    DatePicker("Birthday", selection: $viewModel.birthdate, displayedComponents: .date)
+                    DatePicker("Birthday", selection: $viewModel.user.birthdate, displayedComponents: .date)
                     
                     Button {
                         viewModel.saveChanges()
@@ -31,8 +31,8 @@ struct AccountView: View {
                 }
                 
                 Section(header: Text("Requests")) {
-                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
-                    Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                 }
                 .tint(.accent)
             }
